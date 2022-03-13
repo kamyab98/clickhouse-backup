c3237b6993c291380aec9463d8e97febbbacc0e0
Kamyab Zareh <kamyab.zareh@gmail.com>
2022-03-13 15:59:41 +0330

    feat: Add bandwidth limiter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [[ -v BANDWITH_LIMIT ]]; then
+    tc qdisc add dev eth0 root tbf rate $BANDWITH_LIMIT latency 25ms burst 10k
+fi
+
+
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]] || [[ ! -x  $(command -v "$1") ]]; then
     exec /bin/clickhouse-backup "$@"
 fi
